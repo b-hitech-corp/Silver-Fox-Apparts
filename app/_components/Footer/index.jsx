31410acfc53e18171ad83343cb543ede6ac119f9
yashvanani <vananiyash9828@gmail.com>
2025-06-20 23:31:35 +0530

    added the footer contact icons

--- app/_components/Footer/index.jsx
+++ app/_components/Footer/index.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
+import { FaPhone, FaPhoneVolume } from "react-icons/fa6";
+import { MdLocationOn, MdMail, MdPhoneIphone } from "react-icons/md";
 
 function Footer() {
   return (
@@ -8,11 +10,32 @@ function Footer() {
         <div>
           <h3>Contact Us</h3>
           <ul>
-            <li>SilverFoxApparts@gmail.com</li>
-            <li>(+1)-510-847-1485</li>
-            <li>(228)-71-50-32-32</li>
-            <li>(228)-92-17-16-49</li>
-            <li>Novissi -Rue 42 HDN, Lomé- Togo.</li>
+            <li
+              style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+            >
+              <MdMail />
+              SilverFoxApparts@gmail.com
+            </li>
+            <li
+              style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+            >
+              <MdPhoneIphone /> (+1)-510-847-1485
+            </li>
+            <li
+              style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+            >
+              <FaPhoneVolume /> (228)-71-50-32-32
+            </li>
+            <li
+              style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+            >
+              <FaPhone /> (228)-92-17-16-49
+            </li>
+            <li
+              style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+            >
+              <MdLocationOn /> Novissi -Rue 42 HDN, Lomé- Togo.
+            </li>
             <li className={styles.icons}></li>
           </ul>
         </div>
@@ -35,9 +58,9 @@ function Footer() {
               <Link href={"/contact"}>Contact Us</Link>
             </li>
 
-            <li>
+            {/* <li>
               <Link href="/signin">Guest Area</Link>
-            </li>
+            </li> */}
           </ul>
         </div>
 
